d81798d717523b51ef7b41a4a4aefde70a2172fb
opop8893 <fffyyy810709@gmail.com>
2026-07-29 11:41:08 +0800

    Implement async printQueue function
    Add async function to print items from the queue and mark them as printed.

--- printQueue.js
+++ printQueue.js
@@ -1,1 +1,57 @@
+async function printQueue(){
 
+
+let list =
+
+b21Queue.filter(
+
+x=>!x.printed
+
+);
+
+
+
+for(let item of list){
+
+
+
+let label =
+
+
+"宋爽爽\n\n"+
+
+item.id+
+
+"\n"+
+
+item.name+
+
+"\n"+
+
+(item.cheese?
+"加起司":
+"");
+
+
+
+await printB21(label);
+
+
+
+item.printed=true;
+
+
+}
+
+
+
+saveB21();
+
+
+
+alert(
+"列印完成"
+);
+
+
+}
