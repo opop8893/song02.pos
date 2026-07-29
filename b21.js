2e19e6845bf543a9561c29fee86b399d5dd87794
opop8893 <fffyyy810709@gmail.com>
2026-07-29 11:40:54 +0800

    Update b21.js

--- b21.js
+++ b21.js
@@ -1,1 +1,38 @@
+async function printB21(text){
 
+
+if(!b21Characteristic){
+
+
+alert(
+"請先連線B21"
+);
+
+
+return;
+
+
+}
+
+
+
+let encoder =
+
+new TextEncoder();
+
+
+
+let data =
+
+encoder.encode(text);
+
+
+
+await b21Characteristic.writeValue(
+
+data
+
+);
+
+
+}
